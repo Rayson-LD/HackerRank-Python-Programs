0bb9b07100d337b968a76a3458bbacc30538047e
Rayson L Dsz <72185590+Rayson-LD@users.noreply.github.com>
2021-04-29 09:21:15 +0530

    Update hackerrank.py

--- hackerrank.py
+++ hackerrank.py
@@ -1,6 +1,29 @@
+import math
 import os
+import random
+import re
+import sys
 #1
 if __name__ == '__main__':
     print("Hello World !")
 
 #2
+if __name__ == '__main__':
+    n = int(raw_input().strip())
+if n%2 != 0 :
+    print("Weird")
+if n>=2 and n<=5 :
+    if n%2 == 0 :
+        print("Not Weird")
+        
+if n>=6 and n<=20 :
+    if n%2 == 0 :
+        print("Weird")
+if n>20 :
+    if n%2 == 0 :
+        print("Not Weird")
+
+        
+#3
+
+
