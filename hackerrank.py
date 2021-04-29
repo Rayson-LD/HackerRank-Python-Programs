99541c2fd9aab2445b6f6a8091ff35047e2b9070
Rayson L Dsz <72185590+Rayson-LD@users.noreply.github.com>
2021-04-29 09:25:21 +0530

    Update hackerrank.py

--- hackerrank.py
+++ hackerrank.py
@@ -54,7 +54,22 @@ for i in range(0,n) :
     print(a)
 
 #6
+def is_leap(year):
+    leap = False
+    
+    if year%4 == 0 and year%100 != 0 :
+        leap = True
+        return leap
+    elif year%400 == 0 :
+        leap = True
+        return leap
+    elif year%100 == 0 :
+        leap = False
+        return leap    
+    else :
+        return False
 
+#7
 
 
 
