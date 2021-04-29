b49c026a9b55ab661bfb16574324aaca0a44ad8f
Rayson L Dsz <72185590+Rayson-LD@users.noreply.github.com>
2021-04-29 09:29:27 +0530

    Update hackerrank.py

--- hackerrank.py
+++ hackerrank.py
@@ -85,4 +85,8 @@ if __name__ == '__main__' :
     print ([[a,b,c] for a in range(0,x+1) for b in range(0,y+1) for c in range(0,z+1) if    a + b + c != n ])
     
 #9
+if __name__ == '__main__':
+    n = int(raw_input())
+    lis = list(map(int,raw_input().split()))
+    print(sorted(list(set(lis)))[-2])
 
